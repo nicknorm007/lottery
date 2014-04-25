663710815ba43897f45e756be68cbb832c38eecc
nicknorm007 <nicknorm007@gmail.com>
2014-04-24 21:42:53 -0400

    Initial lottery sim commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Lottery::Application.config.secret_key_base = 'c9eefad0f21c33b5d8b1f2a6510b730a5d439bd217033c418ce39f6320d54bc0d09df61236c4227bd11f30ca75ae47de6516b438e5c239c84cb6450440d19a18'
