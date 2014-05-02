8d44d974e869d5f4331e7371bf6daab149fb4c67
nicknorm007 <nicknorm007@gmail.com>
2014-05-01 22:30:50 -0400

    add days calc

--- app/helpers/lottery_helper.rb
+++ app/helpers/lottery_helper.rb
@@ -4,8 +4,8 @@ module LotteryHelper
   end
 
   def number_of_days_to_play(end_date)
-
-
-    "test string"
+    date_end = Date.strptime(end_date, "%Y-%m-%d")
+    number_of_days = (date_end - Date.today).to_i
+    number_of_days
   end
 end
