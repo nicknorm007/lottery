955c6a5d52635ee3d3c5755b3fa232d9931db2da
nicknorm007 <nicknorm007@gmail.com>
2014-10-05 21:44:20 -0400

    defense stats

--- app/helpers/players_helper.rb
+++ app/helpers/players_helper.rb
@@ -16,6 +16,46 @@ module PlayersHelper
       @next_opp = opts[:next_opp]
     end
 
+    def self.abbreviations
+      abbrev = {NE: 'New England', BUF: 'Buffalo', MIA: 'Miami', NYJ: 'New York',
+                SD: 'San Diego', DEN: 'Denver', KC: 'Kanasas City', OAK: 'Oakland',
+                CIN: 'Cincinnati', CLE: 'Cleveland', BAL: 'Baltimore', PIT: 'Pittsburgh',
+                IND: 'Indianapolis', HOU: 'Houston', TEN: 'Tennessee', JAC: 'Jacksonville',
+                PHI: 'Philadelphia', NYG: 'NY Giants', WAS: 'Washington', DAL: 'Dallas',
+                ARI: 'Arizona', SEA: 'Seattle', SF: 'San Francisco', STL: 'St. Louis',
+                DET: 'Detroit', GB: 'Green Bay', MIN: 'Minnesota', CHI: 'Chicago',
+                CAR: 'Carolina', ATL: 'Atlanta', TB: 'Tampa Bay', NO: 'New Orleans'}
+      abbrev
+    end
+
+  end
+
+  def collect_defensive_league_averages
+    league_averages = []
+    oddshark_url = 'http://www.oddsshark.com/nfl/defensive-stats'
+    page = Nokogiri::HTML(open(oddshark_url))
+    page.css("#chalk tr td.sport_data.league_average").each do |node|
+      text = node.text
+      league_averages.push(text.to_f.round(2) == 0.0 ? text : text.to_f.round(2))
+    end
+    league_averages
+  end
+
+  def collect_team_defensive_stats
+    team_averages = []
+    oddshark_url = 'http://www.oddsshark.com/nfl/defensive-stats'
+    page = Nokogiri::HTML(open(oddshark_url))
+    page.css("#chalk tr > td > a[href*='stats/team/football']").each do |node|
+      team = node.text
+      team_averages.push(team.strip!)
+      ele = node.parent
+      until (ele.next_element.nil?) do
+        tmp = ele.next_element
+        team_averages.push(tmp.text)
+        ele = tmp
+      end
+    end
+    team_averages
   end
 
   def pick_players(opts = {})
@@ -33,15 +73,18 @@ module PlayersHelper
       @total_fppg += @players[pick].fppg
     end
   end
+
   def open_from_file
     f = File.open("files/fanduel.html")
     page = Nokogiri::HTML(f)
     f.close
     page
   end
+
   def simulate_lineup
-    fanduel_url = 'https://www.fanduel.com/e/Game/10648?tableId=5906836&fromLobby=true'
-    page = Nokogiri::HTML(open(fanduel_url))
+    #fanduel_url = 'https://www.fanduel.com/e/Game/10648?tableId=5906836&fromLobby=true'
+    #page = Nokogiri::HTML(open(fanduel_url))
+    page = open_from_file
     @players = []
     @playlist = []
     @total_salary = 0
@@ -49,13 +92,22 @@ module PlayersHelper
     @salary_limit = 60000
     @pppg_target = 90
 
+    dla = collect_defensive_league_averages
+    tds = collect_team_defensive_stats
     page.css("tr[id*='playerListPlayerId']").each do |tr|
       pos, name, fppg, played, fixture, salary = tr.xpath('./td')
       salary = salary.text.gsub(/\D/,'').to_i
       fppg = fppg.text.to_i
-      opposing_team = fixture.to_html.split("b>")[1].gsub('</','')
+      opposing_team = fixture.to_html.split("@")
+      if (opposing_team[1] =~ /<b>/)
+        opp = opposing_team[0].split(">")[1]
+      else
+        opp = opposing_team[1].split("<")[0]
+      end
+      team_opp = SportsPlayer.abbreviations[opp.to_sym]
       p = SportsPlayer.new(name: name.text, pos: pos.text, fppg: fppg, fixture: fixture.text, salary: salary,
-                           next_opp: opposing_team)
+                           next_opp: opp, opp_ypl: xxx, la_ypl: xxx, opp_ypr: xxx, la_ypr: xxx,
+                           opp_ypp: xxx, la_ypp: xxx)
       @players.push(p)
     end
 
@@ -74,7 +126,3 @@ module PlayersHelper
   end
 
 end
-
-#for url access use below
-#fanduel_url = 'https://www.fanduel.com/e/Game/10648?tableId=5906836&fromLobby=true'
-#page = Nokogiri::HTML(open(fanduel_url))
