87fb37c3edc4a70a9c7bf1ea33c31d05df360b6f
nicknorm007 <nicknorm007@gmail.com>
2014-12-29 20:20:07 -0500

    prod changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -25,7 +25,7 @@ Lottery::Application.configure do
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
-  
+
   # Generate digests for assets URLs.
   config.assets.digest = true
 
